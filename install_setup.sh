a22a2f99f1d348ae19296010fd5c3fa50ec7331f
GNUS <60074926+theMark001@users.noreply.github.com>
2024-12-08 23:14:06 +0100

    issue config folder --at1

--- install_setup.sh
+++ install_setup.sh
@@ -8,20 +8,25 @@ xcode-select --install
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# # Turn off Desktop
-# defaults write com.apple.finder CreateDesktop false; killall Finder
+# Turn off Desktop
+defaults write com.apple.finder CreateDesktop false; killall Finder
 
-# # Cloning config files 
-# git clone https://github.com/theMark001/MacOS-Ricing.git ~/Downloads/MacOS-Ricing
+# Cloning config files 
+git clone https://github.com/theMark001/MacOS-Ricing.git ~/Downloads/MacOS-Ricing
 
-# chmod +x ~/Downloads/MacOS-Ricing/loop.sh 
-# chmod +x ~/Downloads/MacOS-Ricing/raycast.sh 
-# chmod +x ~/Downloads/MacOS-Ricing/sketchybar.sh 
-# chmod +x ~/Downloads/MacOS-Ricing/skhd.sh 
-# chmod +x ~/Downloads/MacOS-Ricing/yabai.sh
+# Creating config dir
+mkdir -p ~/.config
 
-# # bash ~/Downloads/MacOS-Ricing/loop.sh
-# # bash ~/Downloads/MacOS-Ricing/raycast.sh
-# bash ~/Downloads/MacOS-Ricing/sketchybar.sh
-# # bash ~/Downloads/MacOS-Ricing/skhd.sh
-# # bash ~/Downloads/MacOS-Ricing/yabai.sh
+# Make all file executable
+chmod +x ~/Downloads/MacOS-Ricing/loop.sh 
+chmod +x ~/Downloads/MacOS-Ricing/raycast.sh 
+chmod +x ~/Downloads/MacOS-Ricing/sketchybar.sh 
+chmod +x ~/Downloads/MacOS-Ricing/skhd.sh 
+chmod +x ~/Downloads/MacOS-Ricing/yabai.sh
+
+# Run files
+bash ~/Downloads/MacOS-Ricing/loop.sh
+bash ~/Downloads/MacOS-Ricing/raycast.sh
+bash ~/Downloads/MacOS-Ricing/sketchybar.sh
+bash ~/Downloads/MacOS-Ricing/skhd.sh
+bash ~/Downloads/MacOS-Ricing/yabai.sh
