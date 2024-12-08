abe6c89b97569f5fb42f7a18938a803c99e302bf
GNUS <60074926+theMark001@users.noreply.github.com>
2024-12-08 18:49:03 +0100

    one click --test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-test
+```bash
+curl -L https://github.com/theMark001/MacOS-Ricing/raw/refs/heads/main/install_setup.sh | sh
+```
---
+++ install_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod +x pre_settings.sh loop.sh raycast.sh sketchybar.sh skhd.sh yabai.sh
+
+bash pre_settings.sh
+# bash loop.sh
+# bash raycast.sh
+bash sketchybar.sh
+# bash skhd.sh
+# bash yabai.sh
