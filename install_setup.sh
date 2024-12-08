ac6df88943c6bbb184ecbb0c00be3c700cbddcb0
GNUS <60074926+theMark001@users.noreply.github.com>
2024-12-08 21:25:02 +0100

    cloning, executing right path

--- install_setup.sh
+++ install_setup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-chmod +x pre_settings.sh loop.sh raycast.sh sketchybar.sh skhd.sh yabai.sh
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-bash pre_settings.sh
+# Turn off Desktop
+defaults write com.apple.finder CreateDesktop false; killall Finder
+
+# Cloning config files 
+git clone https://github.com/theMark001/MacOS-Ricing.git ~/Downloads/MacOS-Ricing
+
+chmod +x ~/Downloads/MacOS-Ricing/loop.sh 
+chmod +x ~/Downloads/MacOS-Ricing/raycast.sh 
+chmod +x ~/Downloads/MacOS-Ricing/sketchybar.sh 
+chmod +x ~/Downloads/MacOS-Ricing/skhd.sh 
+chmod +x ~/Downloads/MacOS-Ricing/yabai.sh
+
 # bash loop.sh
 # bash raycast.sh
 bash sketchybar.sh
