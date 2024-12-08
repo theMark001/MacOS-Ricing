ffaed22dd198464e8a5238ca115b242ac7519673
GNUS <60074926+theMark001@users.noreply.github.com>
2024-12-08 23:46:37 +0100

    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,34 @@
+# Add here the default system settings
+
+#!/bin/bash
+
+brew uninstall loop
+brew untap FelixKratz/formulae
+brew uninstall sketchybar
+brew uninstall jq
+brew uninstall lua
+brew uninstall switchaudio-osx
+brew uninstall nowplaying-cli
+brew uninstall --cask sf-symbols
+brew uninstall --cask font-sf-mono
+brew uninstall --cask font-sf-pro
+brew uninstall skhd
+brew uninstall yabai
+
+rm -rf "$HOME/Library/Fonts/sketchybar-app-font.ttf"
+rm -rf ~/Downloads/MacOS-Ricing
+rm -rf ~/.config
+
+sed -i '' '/eval "\$(\/opt\/homebrew\/bin\/brew shellenv)"/d' ~/.zprofile
+defaults write com.apple.finder CreateDesktop true; killall Finder
+
+# Verification commands
+brew list | grep loop
+brew tap | grep FelixKratz/formulae
+brew list | grep -E 'sketchybar|jq|lua|switchaudio-osx|nowplaying-cli'
+brew list --cask | grep -E 'sf-symbols|font-sf-mono|font-sf-pro'
+brew list | grep -E 'skhd|yabai'
+ls "$HOME/Library/Fonts/" | grep sketchybar-app-font.ttf
+ls ~/Downloads | grep MacOS-Ricing
+ls -a ~ | grep .config
+grep 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.zprofile
